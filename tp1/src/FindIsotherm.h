2a588e8a135431a8f2bb2f02f9df40580681aca3
ecoronel <ezequiel.f.coronel@gmail.com>
2022-04-09 18:31:21 -0300

    Split BuildEqSystem into BuildMatrix and BuildVector

--- tp1/src/FindIsotherm.h
+++ tp1/src/FindIsotherm.h
@@ -3,24 +3,22 @@
 #include "BackwardSustitution.h"
 #include "ForwardSubstitution.h"
 #include "GaussianElimination.h"
+#include "LUFactorization.h"
 #include "MatrixN.h"
 #include "Parameters.h"
 #include "VectorN.h"
-#include "LUFactorization.h"
 
 namespace mn
 {
     constexpr float pi = 3.141592653589793f;
 
-    std::pair<MatrixNf, VectorNf> BuildEqSystem(float ri, float re, int m, int n, const std::vector<float>& internalTemps, const std::vector<float>& externalTemps)
+    MatrixNf BuildMatrix(float ri, float re, int m, int n)
     {
         auto A = MatrixNf::zeros(n * (m - 1));
-        auto b = VectorNf::zeros(n * (m - 1));
         
         const float deltaTheta = 2.0f * pi / static_cast<float>(n);
         const float sqDeltaTheta = deltaTheta * deltaTheta;
 
-        // const double deltaRadius = (re - ri) / (static_cast<double>(m) + 1.0);
         const float deltaRadius = (re - ri) / static_cast<float>(m);
         const float sqDeltaRadius = deltaRadius * deltaRadius;
 
@@ -31,8 +29,6 @@ namespace mn
             const int j = 1;
             const int row = k;
             const float sqRadius = radius * radius;
-
-            b(k) = -(internalTemps[k] * (sqRadius - radius * deltaRadius ) * sqDeltaTheta );
 
             A(row, (n + ((k-1)%n)) % n) = sqDeltaRadius;
 
@@ -54,8 +50,6 @@ namespace mn
             A(row, n*(m-2) + (n + ((k-1)%n)) % n) = sqDeltaRadius;
             A(row, row) = -2.0f*sqRadius*sqDeltaTheta + radius*deltaRadius*sqDeltaTheta - 2.0f*sqDeltaRadius;
             A(row, n*(m-2) + (n + ((k+1)%n)) % n) = sqDeltaRadius;
-
-            b(row) = -externalTemps[k]*sqRadius*sqDeltaTheta;
         }
 
         for (int j = 2; j < m - 1; ++j)
@@ -74,14 +68,42 @@ namespace mn
             }
         }
 
-        return {A, b};
+        return A;
+    }
+
+    VectorNf BuildVector(float ri, float re, int m, int n, const std::vector<float>& internalTemps, const std::vector<float>& externalTemps)
+    {
+        auto b = VectorNf::zeros(n * (m - 1));
+        
+        const float deltaTheta = 2.0f * pi / static_cast<float>(n);
+        const float sqDeltaTheta = deltaTheta * deltaTheta;
+        const float deltaRadius = (re - ri) / static_cast<float>(m);
+
+        // caso especial j = 1
+        float radius = ri+deltaRadius;
+        for (int k = 0; k < n; ++k)
+        {
+            const float sqRadius = radius * radius;
+            b(k) = -(internalTemps[k] * (sqRadius - radius * deltaRadius ) * sqDeltaTheta );
+        }
+
+        // caso especial j = m - 1
+        radius = re - deltaRadius;
+        for (int k = 0; k < n; ++k)
+        {
+            const float sqRadius = radius * radius;
+            b(n*(m - 2) + k) = -externalTemps[k]*sqRadius*sqDeltaTheta;
+        }
+
+        return b;
     }
 
     inline void FindIsotherm(const int method, const InputParams& input, OutputParams& output)
     {
+        auto A = BuildMatrix(input.ri, input.re, input.m, input.n);
         for (const Instance& inst: input.instances)
         {
-            auto [A, b] = BuildEqSystem(input.ri, input.re, input.m, input.n, inst.internalTemps, inst.externalTemps);
+            auto b = BuildVector(input.ri, input.re, input.m, input.n, inst.internalTemps, inst.externalTemps);
             auto x = VectorNf::zeros(b.size());
 
             if (method == 0)
--- tp1/src/tests/FindIsothermTest.cpp
+++ tp1/src/tests/FindIsothermTest.cpp
@@ -8,22 +8,46 @@ namespace
     constexpr float pi = 3.141592653589793f;
 }
 
-TEST(FindIsothermTest, BuildEqSystem)
+TEST(FindIsothermTest, BuildMatrix)
 {
-    // 4.5 5.5 5 6 500 1
-    // 1500.0 1500.0 1500.0 1500.0 1500.0 1500.0 50.0 50.0 50.0 50.0 50.0 50.0
-
     const int n = 6;
     const int m = 4;
     const float ri = 4.0f;
     const float re = 5.0f;
 
-    auto [A, b] = mn::BuildEqSystem(ri, re, m, n, 
-        {1500.0f, 1500.0f, 1500.0f, 1500.0f, 1500.0f, 1500.0f},
-        {50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f});
+    auto A = mn::BuildMatrix(ri, re, m, n);
     
     EXPECT_EQ(A.rows(), (m - 1) * n );
     EXPECT_EQ(A.cols(), (m - 1) * n );
+
+    const float r1 = 4.25;
+    const float delta_r = (re - ri) / static_cast<float>(m);
+    const float sqDelta_r = delta_r * delta_r;
+
+    const float delta_t = 2.0f*pi / static_cast<float>(n);
+    const float sqDelta_t = delta_t*delta_t;
+    
+    // matriz
+    EXPECT_FLOAT_EQ(A(0, 0),  -2.0f*r1*r1*sqDelta_t + r1*delta_r*sqDelta_t - 2.0f*sqDelta_r );
+    EXPECT_FLOAT_EQ(A(0, 1), sqDelta_r);
+    EXPECT_FLOAT_EQ(A(0, 2),  0.0f);
+    EXPECT_FLOAT_EQ(A(0, 3),  0.0f);
+    EXPECT_FLOAT_EQ(A(0, 4),  0.0f);
+    EXPECT_FLOAT_EQ(A(0, 5), sqDelta_r);
+    EXPECT_FLOAT_EQ(A(0, 6), r1*r1*sqDelta_t);
+}
+
+TEST(FindIsothermTest, BuildVector)
+{
+    const int n = 6;
+    const int m = 4;
+    const float ri = 4.0f;
+    const float re = 5.0f;
+
+    auto b = mn::BuildVector(ri, re, m, n, 
+        { 1500.0f, 1500.0f, 1500.0f, 1500.0f, 1500.0f, 1500.0f },
+        { 50.0f, 50.0f, 50.0f, 50.0f, 50.0f, 50.0f });
+    
     EXPECT_EQ(b.size(), (m - 1) * n );
 
     const float r1 = 4.25;
@@ -40,8 +64,6 @@ TEST(FindIsothermTest, BuildEqSystem)
     {
         EXPECT_FLOAT_EQ(b(i),  b0);
     }
-    
-    // TODO check zeros
 
     const float r3 = 4.75f;
     const float bnm2 = -50.f*(r3*r3*sqDelta_t);
@@ -49,19 +71,6 @@ TEST(FindIsothermTest, BuildEqSystem)
     {
         EXPECT_FLOAT_EQ(b(n*(m - 2) + i),  bnm2);
     }
-    
-    // matriz
-    EXPECT_FLOAT_EQ(A(0, 0),  -2.0f*r1*r1*sqDelta_t + r1*delta_r*sqDelta_t - 2.0f*sqDelta_r );
-    EXPECT_FLOAT_EQ(A(0, 1), sqDelta_r);
-    EXPECT_FLOAT_EQ(A(0, 2),  0.0f);
-    EXPECT_FLOAT_EQ(A(0, 3),  0.0f);
-    EXPECT_FLOAT_EQ(A(0, 4),  0.0f);
-    EXPECT_FLOAT_EQ(A(0, 5), sqDelta_r);
-    EXPECT_FLOAT_EQ(A(0, 6), r1*r1*sqDelta_t);
-    
-    // std::cout << b << '\n';
-    // std::cout << std::fixed << std::setprecision(3) << A << '\n';
-
 }
 
 TEST(FindIsothermTest, FindIsothermGE)
@@ -83,9 +92,9 @@ TEST(FindIsothermTest, FindIsothermGE)
     mn::OutputParams output;
     mn::FindIsotherm(0, input, output);
 
-    EXPECT_FLOAT_EQ(output.soluciones[0](0), 1500.0f);
-    EXPECT_FLOAT_EQ(output.soluciones[0](6), 750.0f);
-    EXPECT_FLOAT_EQ(output.soluciones[0](17), 100.0f);
+    EXPECT_FLOAT_EQ(output.soluciones[0](0), 1105.1381);
+    EXPECT_FLOAT_EQ(output.soluciones[0](6), 733.50342);
+    EXPECT_FLOAT_EQ(output.soluciones[0](17), 382.51532);
 }
 
 TEST(FindIsothermTest, FindIsothermLU)
@@ -107,9 +116,7 @@ TEST(FindIsothermTest, FindIsothermLU)
     mn::OutputParams output;
     mn::FindIsotherm(1, input, output);
 
-    EXPECT_FLOAT_EQ(output.soluciones[0](0), 1500.0f);
-    EXPECT_FLOAT_EQ(output.soluciones[0](6), 750.0f);
-    EXPECT_FLOAT_EQ(output.soluciones[0](17), 100.0f);
-
-    std::cout << output.soluciones[0] ;
+    EXPECT_FLOAT_EQ(output.soluciones[0](0), 1105.1381);
+    EXPECT_FLOAT_EQ(output.soluciones[0](6), 733.50342);
+    EXPECT_FLOAT_EQ(output.soluciones[0](17), 382.51532);
 }
