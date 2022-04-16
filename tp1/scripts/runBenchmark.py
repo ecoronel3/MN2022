9e3345ea3d234d7449ce829d2d0b46edac67d0a3
ecoronel <ezequiel.f.coronel@gmail.com>
2022-04-16 16:10:08 -0300

    exp - results

--- tp1/scripts/runBenchmark.py
+++ tp1/scripts/runBenchmark.py
@@ -3,31 +3,35 @@ import os
 import json
 from datetime import datetime
 
-def main():
-    now = datetime.now()
-    currentTime = now.strftime("%Y%m%d_%H%M%S")
+pathToExe = "../../build/Release/BenchmarkEGvsLU.exe"
 
-    pathToExe = "../../build/Release/BenchmarkEGvsLU.exe"
-    repeat = 50
-
-    inputFilesDir = '../exp/testEGvsLU/input/'
+def process(inputFilesDir, repeat):
     results = []
 
     for filename in os.listdir(inputFilesDir):
         input_file = os.path.join(inputFilesDir, filename)
         if os.path.isfile(input_file) and input_file.endswith(".in"):
-            datetime.now()
             print("[{0:}] processing {1:}".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S"), filename))
-
             process = sp.Popen([pathToExe, input_file, str(repeat)], stdout=sp.PIPE)
             (output, err) = process.communicate()
             exitCode = process.wait()
             resultStr = output.decode('utf-8')
             results.append(json.loads(resultStr))
 
-    outputFilename = '../exp/testEGvsLU/results_{:}.json'.format(currentTime)
+    return results
+
+def main(inputFilesDir, set, repeat = 50):
+    now = datetime.now()
+    currentTime = now.strftime("%Y%m%d_%H%M%S")
+    outputFilename = '../exp/testEGvsLU/results{:}_{:}.json'.format(set, currentTime)
+    results = process(inputFilesDir, repeat)
+    print("[{0:}] finished.".format(datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     with open(outputFilename,'w', encoding='utf-8') as outputFile:
         outputFile.write(json.dumps(results, indent=4))
 
 if __name__ == '__main__':
-    main()
+    main(inputFilesDir = '../exp/testEGvsLU/input2/', set=2)
+    main(inputFilesDir = '../exp/testEGvsLU/input3/', set=3)
+    main(inputFilesDir = '../exp/testEGvsLU/input4/', set=4)
+    main(inputFilesDir = '../exp/testEGvsLU/input5/', set=5)
+
