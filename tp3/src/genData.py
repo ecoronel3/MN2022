96ba3ac56b9607f71c0352fe3e96d126b55c265b
ecoronel <ezequiel.f.coronel@gmail.com>
2022-06-22 21:32:46 -0300

    Synth data 2

--- tp3/src/genData.py
+++ tp3/src/genData.py
@@ -3,29 +3,29 @@ import numpy as np
 import math
 import csv
 
-def generate_data(n_samples, m_features):    
-    features = []
+def func_g(x1, x2, x3):
+    return x1*x2 + x1*x3
+
+def generate_data(n_samples,):
     mu=0
     sigma=1
-    X = [x for x in range(1, n_samples + 1) ]    
-    for feature_generator in m_features:
-        feature_values = np.array(list(map(feature_generator, X)))
-        feature_values = feature_values + np.random.normal(mu, sigma, size=n_samples)
-        features.append(feature_values)
+    x1 = np.random.uniform(0, 10, n_samples)
+    x2 = np.random.exponential(2, n_samples)
+    x3 = np.random.normal(10, 5, n_samples)
+    y = func_g(x1, x2, x3) + np.random.normal(mu, sigma, size=n_samples)
+    return [y, x1, x2, x3]
 
-    return features
+def generate_file(n_samples, filepath):
+    features = generate_data(n_samples)
 
-def generate_file(n_samples, m_features, filepath):
-    features = generate_data(n_samples, m_features)
-
-    header = ['id', 'X']    
-    for feature_id in range(1, len(m_features) + 1):
-        header.append('feature_' + str(feature_id))
+    header = ['id', 'Y']
+    for feature_id in range(1, len(features)):
+        header.append('X' + str(feature_id))
 
     data = []
     for sample_id in range(0, n_samples):
-        r = [str(sample_id), sample_id]
-        for feature_id in range(0, len(m_features)):
+        r = [str(sample_id)]
+        for feature_id in range(0, len(features)):
             r.append( round(features[feature_id][sample_id], 3) )
 
         data.append(r)
@@ -36,13 +36,7 @@ def generate_file(n_samples, m_features, filepath):
         writer.writerows(data)
 
 def main():
-    functors = [
-            lambda x: 0.2*x,
-            lambda x: 4.0*math.sin(math.pi * x/64),
-            lambda x: math.sqrt(x)*0.5
-            ]
-
-    generate_file(150, functors, 'data/synthData.csv')
+    generate_file(150, 'data/synthData.csv')
 
 if __name__ == '__main__':
     main()
