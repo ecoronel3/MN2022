a1869faeaaf58eff3050b645166ded1ea6b24461
Geronimo Romczyk <g.romczyk@hotmail.com>
2022-07-13 20:04:14 -0300

    agregue los cambios al script

--- tp3/src/tp3.py
+++ tp3/src/tp3.py
@@ -13,8 +13,9 @@ def tricubic(x):
     return y
 
 def normalized(array):
-    stds = array.std(axis=0)    
-    return (array- array.mean(axis=0))/stds
+    stds = array.std(axis=0)
+    
+    return (array- array.mean(axis=0))/stds, (est - array.mean(axis=0))/stds
 
 def get_indexes(distances, q):
     mins = []
@@ -24,57 +25,51 @@ def get_indexes(distances, q):
                 heapq.heapreplace(mins, (-1*distances[i], i))
         else:
             heapq.heappush(mins, (-1*distances[i], i))
-
+    
     indexes = []
     max_dist = 0.0
     for (dist, index) in mins:
         indexes.append(index)
+        
         if -1*dist > max_dist:
             max_dist = -1*dist
+            
     return indexes, max_dist
 
 def get_weights(distances, max_dist):
     weights = tricubic(distances/max_dist)
     return np.diag(weights)
 
-def loess(data, Y, f=0.1, fit=1, dist=2, normalize=True):
-    cant = len(Y)
-    q = int(math.ceil(f*cant))
+def loess(data, Y, to_estimate, f=0.1, fit=1, dist=2, normalize=True):
+    q = int(math.ceil(f*len(Y)))
     if normalize:
-        data = normalized(data)
-    else:
-        data = data
-    
-    y_ests = np.zeros_like(Y,dtype="float64")
-    poly = PolynomialFeatures(fit)
-    
-    for i in range(0, cant):
-        distances = linalg.norm(data - data[i], ord=dist, axis=1)
-        indexes, max_dist = get_indexes(distances, q)
-        W = get_weights(distances[indexes], max_dist)
-        b = Y[indexes]
-        A = poly.fit_transform(data[indexes])
-            
-        At = np.transpose(A)
-        try:
-            coeffs = linalg.solve(np.dot(At, np.dot(W, A)), np.dot(At, np.dot(W, b)))
-        except:
-
-            U, E, Vt = linalg.svd(np.dot(W, A))
-            V = np.transpose(Vt)
-            Ut = np.transpose(U)
-            
-            Et = np.zeros((np.shape(V)[1], np.shape(Ut)[0]))
-            
-            for j in range(0, len(E)):
-                if E[j] != 0:
-                    Et[j,j] = 1/E[j]
-            
-            MPInv = np.dot(V, np.dot(Et, Ut))
-            coeffs = np.dot(MPInv, np.dot(W, b))
+        data, to_estimate = normalized(data, to_estimate)
         
-        y_est = np.dot(poly.fit_transform(data[i].reshape(1, -1)),coeffs)
+    
+    poly = PolynomialFeatures(fit)
+    cant = len(to_estimate)
+    
+    y_ests = np.zeros(cant,dtype="float64")
+    
+    for i in range(cant):
+        distances = linalg.norm(data - to_estimate[i], ord=dist, axis=1)
+        indexes, max_dist = get_indexes(distances, q)
 
+        A = poly.fit_transform(data[indexes])
+        x = poly.transform(to_estimate[i].reshape(1,-1))
+
+        W = get_weights(distances[indexes], max_dist)
+
+        b = Y[indexes]
+        
+        atw = A.T@W
+
+        coeffsSinB = linalg.pinv(atw@A)@atw
+        coeffs = coeffsSinB@b
+        
+        l = x@coeffsSinB
+
+        y_est = np.dot(coeffs, x[0])
         y_ests[i] = y_est
     
     return y_ests
